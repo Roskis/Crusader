e3d642ac21703e8c8be2d897ca173c164967af42
Antti Karkinen <antti.karkinen@aalto.fi>
2015-03-08 18:13:03 +0200

    Moved Direction and Gamestate to Helpers.

--- Crusader/src/game/GameState.scala
+++
@@ -1,9 +0,0 @@
-package game
-
-/** There are three states of game */
-object GameState extends Enumeration {
-
-  type GameState = Value
-  val GAME, MAIN_MENU, CHARACTER_CREATION, LEVEL = Value
-
-}
--- Crusader/src/game/Helpers.scala
+++ Crusader/src/game/Helpers.scala
@@ -5,6 +5,98 @@ import org.newdawn.slick.util.ResourceLoader
 import org.newdawn.slick.opengl.{Texture, TextureLoader}
 import java.io.{IOException, InputStream}
 import scala.collection.mutable.Buffer
+import Math.abs
+
+/** There are three states of game */
+object GameState extends Enumeration {
+
+  type GameState = Value
+  val GAME, MAIN_MENU, CHARACTER_CREATION, LEVEL = Value
+
+}
+
+/** There are four main directions and four intermediate directions */
+object Direction extends Enumeration {
+
+  private val rnd = getRnd
+  
+  type Direction = Value
+  val N, NE, E, SE, S, SW, W, NW = Value
+
+  /** Method to get new coordinates when moving to the wanted direction.
+   *
+   * @param dir is the given direction
+   * @param x is the current x coordinate
+   * @param y is the current y coordinate
+   * @return Tuple2 containing new x and y coordinates
+   */
+  def getCoordinates(dir: Direction, x: Int, y: Int): Coordinate = {
+    dir match {
+      case d if (d == N) => new Coordinate(x, y-1)
+      case d if (d == E) => new Coordinate(x+1, y)
+      case d if (d == S) => new Coordinate(x, y+1)
+      case d if (d == W) => new Coordinate(x-1, y)
+      case d if (d == NE) => new Coordinate(x+1, y-1)
+      case d if (d == NW) => new Coordinate(x-1, y-1)
+      case d if (d == SE) => new Coordinate(x+1, y+1)
+      case d if (d == SW) => new Coordinate(x-1, y+1)
+    }
+  }
+  
+  def getCoordinates(dir: Direction, startCoord: Coordinate): Coordinate = {
+    getCoordinates(dir: Direction, startCoord.getX, startCoord.getY)
+  }
+  def getCoordinates(dir: Direction, tile: Tile): Coordinate = {
+    getCoordinates(dir: Direction, tile.getX, tile.getY)
+  }
+  def getCoordinates(dir: Direction, obj: Object): Coordinate = {
+    getCoordinates(dir: Direction, obj.getX, obj.getY)
+  }
+  
+  /** Method to get the direction when looking from start coordinate to end coordinate */
+  def getDirection(startCoord: Coordinate, endCoord: Coordinate): Direction = {
+    var dx = endCoord.getX - startCoord.getX
+    var dy = endCoord.getY - startCoord.getY
+    var dir: Direction = null
+    if (abs(dx) >= abs(dy)) {
+      if (dx >= 0) {
+        dir = E
+      }
+      else {
+        dir = W
+      }
+    }
+    else {
+      if (dy >= 0) {
+        dir = S
+      }
+      else {
+        dir = N
+      }
+    }
+    dir
+  }
+  
+  /** Return random direction. Insert 4 if you want to move only with main directions and 8 if you 
+   *  want to allow intermediate directions.
+   *
+   * @param num number of directions allowed
+   * @return Direction random direction
+   */
+  def randomDirection(num: Int): Direction = {
+    rnd.nextInt(num) match {
+      case r if (r == 0) => N
+      case r if (r == 1) => E
+      case r if (r == 2) => S
+      case r if (r == 3) => W
+      case r if (r == 4) => NW
+      case r if (r == 5) => NE
+      case r if (r == 6) => SW
+      case r if (r == 7) => SE
+    }
+  }
+  
+}
 
 /** Methods that don't anywhere but are needed everywhere */
 object Helpers {
