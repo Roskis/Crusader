13330ddd90f719a0585a8c51081430efc24c6d70
Antti Karkinen <antti.karkinen@aalto.fi>
2015-03-12 16:34:47 +0200

    Lowered Java requirements to 1.6 and added linux and mac support.

--- Crusader/src/game/Launcher.java
+++ Crusader/src/game/Launcher.java
@@ -5,7 +5,15 @@ import java.nio.file.Paths;
 // Java launcher for game
 public class Launcher {
     public static void main(String[] args) {
-    	System.setProperty("org.lwjgl.librarypath", Paths.get("").toAbsolutePath() + "/lib/native/windows/");
+    	if (System.getProperty("os.name").contains("Windows")) {
+    		System.setProperty("org.lwjgl.librarypath", Paths.get("").toAbsolutePath() + "/lib/native/windows/");
+    	} else if (System.getProperty("os.name").contains("Mac")) {
+    		System.setProperty("org.lwjgl.librarypath", Paths.get("").toAbsolutePath() + "/lib/native/macosx/");
+    	} else if (System.getProperty("os.name").contains("Linux")) {
+    		System.setProperty("org.lwjgl.librarypath", Paths.get("").toAbsolutePath() + "/lib/native/linux/");
+    	} else {
+    		throw new RuntimeException("Your OS is not supported");
+    	}
         Main.main(args);
     }
 }
